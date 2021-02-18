f4f9a7881b59639c2c05c72f22e7fc4a90ff8dc9
AngelMontoya9 <74206518+AngelMontoya9@users.noreply.github.com>
2021-02-18 14:25:31 -0600

    Add files via upload

---
+++ ifelse/src/main/java/paquete/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author HP
+ */
+class Main {
+    public static void main(String [] args){
+        int var=6;
+        int var2=10;
+        if(!((var==6) || (var2>5))){
+            System.out.println("Tu operacion es verdadera");
+        }else{
+            System.out.println("Tu operacion es falsa");
+        }
+    }
+}
