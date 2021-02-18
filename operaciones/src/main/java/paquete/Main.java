0cba56bddbc38869433999e39c4c48e2f18f2595
AngelMontoya9 <74206518+AngelMontoya9@users.noreply.github.com>
2021-02-18 14:16:38 -0600

    Add files via upload

---
+++ operaciones/src/main/java/paquete/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author HP
+ */
+class Main {
+    public static void main(String [] args){
+        short suma=3+10;
+        int resta=8-17;
+        long residuo=9/2;
+        float multiplicacion=2*(15*(-2));
+        double division=10/3.4;
+        System.out.println("El resultado de la suma es: "+suma);
+        System.out.println("El resultado de la resta es: "+resta);
+        System.out.println("El resultado de la multiplicacion es: "+multiplicacion);
+        System.out.println("El resultado de la division es: "+division);
+        System.out.println("El resultado del resudio es: "+residuo);
+    }
+}
