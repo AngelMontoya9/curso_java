fc0b3b6249b799d85999d3bfc1a2d7c38669a2d7
AngelMontoya9 <74206518+AngelMontoya9@users.noreply.github.com>
2021-02-19 10:01:02 -0600

    Add files via upload

---
+++ arreglos/src/main/java/paquete/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author HP
+ */
+class Main {
+    public static void main(String [] args){
+        String s[];
+        Object q[];
+        int x[]={10,5,7};
+        for(int i=0;i<x.length;i++){
+            System.out.println("Arreglo en indice:"+i+"es igual a:"+x[i]);
+        }
+    }
+    
+}
