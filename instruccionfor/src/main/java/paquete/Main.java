48ec3aa18ff6ba38a347ccbffc33a561d74c0a03
AngelMontoya9 <74206518+AngelMontoya9@users.noreply.github.com>
2021-02-18 21:41:12 -0600

    Add files via upload

---
+++ instruccionfor/src/main/java/paquete/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author HP
+ */
+class Main {
+    public static void main(String [] args){
+        for(int contador=1; contador<=20;contador++){
+            contador=contador*2;
+            System.out.println("El contador vale: "+contador);
+        }
+    }
+}
